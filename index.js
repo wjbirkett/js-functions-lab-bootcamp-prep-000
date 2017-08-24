2396df48ee5cbcfe0221b5c1c102a3f172a7391d
William Birkett <williebirkett@gmail.com>
2017-08-24 20:16:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-var name = "Willie"
-
 function happyHolidays() {
   return 'Happy holidays!'
 }
 
-
+var name = "Willie";
 
 function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!'
+  return 'Happy holidays, ${name}!';
 }
